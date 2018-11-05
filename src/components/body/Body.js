131c5e7f91e45f53cc2c68fb22d70150cb00821d
Mashkour <azizmashkour@gmail.com>
2018-11-05 17:43:15 +0100

    feat: generate human body component and add main body image

---
+++ src/components/body/Body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import BodyGard from './human-body.png';
+export const Body = ({ d3 }) => (
+  <div className="Body-Container">
+    <img src={BodyGard} className="Human-image" alt="Logo" />
+  </div>
+);
+
+export default (Body);
