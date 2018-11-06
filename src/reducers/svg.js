f491b954bff071bcc24d0dfc26463ddfcdaae8fe
Mashkour <azizmashkour@gmail.com>
2018-11-06 12:17:59 +0100

    feat: make sure we get the default svg from action payload

---
+++ src/reducers/svg.js
@@ -0,0 +1,17 @@
+import { APPEND_SVG } from '../actions/types';
+
+const INITIAL_STATE = {
+  svg: null,
+};
+
+export default function(state=INITIAL_STATE, action) {
+  switch (action.type) {
+    case APPEND_SVG:
+      return {
+        ...state,
+        svg: action.payload,
+      };
+    default:
+      return state;
+  }
+}
