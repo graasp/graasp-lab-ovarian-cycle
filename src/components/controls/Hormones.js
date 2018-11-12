51cad2b2f1566ffb41afd939949ed18d82819e83
Mashkour <azizmashkour@gmail.com>
2018-11-12 14:22:00 +0100

    feat: make sure the animate-hormone class is only added on time

--- src/components/controls/Hormones.js
+++ src/components/controls/Hormones.js
@@ -1,27 +1,35 @@
 import React from 'react';
 
-export const Hormones = () => (
+export const Hormones = ({ secretLhFsh, secretProgest, secretOestro }) => (
   <div className="Hormone-Container">
     <div className="hormones-container">
       <p>
-        <span className="fsh-puces">.</span>
-        <span className="fsh-container">FSH</span>
-        <span className="fsh-state">-</span>
-      </p>
-      <p>
         <span className="lh-puces">.</span>
         <span className="lh-container">LH</span>
-        <span className="lh-state">-</span>
+        <span className={`${secretLhFsh ? 'animate-hormones' : ''} lh-state`}>
+          {secretLhFsh ? '+' : '-'}
+        </span>
+      </p>
+      <p>
+        <span className="fsh-puces">.</span>
+        <span className="fsh-container">FSH</span>
+        <span className={`${secretLhFsh ? 'animate-hormones' : ''} fsh-state`}>
+          {secretLhFsh ? '+' : '-'}
+        </span>
       </p>
       <p>
         <span className="oestro-puces">.</span>
         <span className="oestro-container">OESTROGÈNES</span>
-        <span className="oestro-state"></span>
+        <span className={`${secretOestro ? 'animate-hormones' : ''} oestro-state`}>
+          {secretOestro ? '+' : ''}
+        </span>
       </p>
       <p>
         <span className="progest-puces">.</span>
         <span className="progest-container">PROGESTÉRONES</span>
-        <span className="progest-state"></span>
+        <span className={`${secretProgest ? 'animate-hormones' : ''} progest-state`}>
+          {secretProgest ? '+' : ''}
+        </span>
       </p>
     </div>
   </div>
