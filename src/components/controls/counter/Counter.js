b5f7b90156a64ef18a043c89a7c8d5617b85d4f7
Mashkour <azizmashkour@gmail.com>
2018-11-20 17:33:45 +0100

    fix: separate counter component from it test and styles files

---
+++ src/components/controls/counter/Counter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Counter.css';
+import { Button } from 'reactstrap';
+
+export const Counter = ({
+  handleStart,
+  handleStop,
+  seconds }) => (
+  <div className="Counter-Container">
+    <div className="clock-setter">
+      <p className="select-step"> ...or launch the cycle </p>
+      <Button className='Button-start' onClick={handleStart}>Start cycle</Button>
+      &nbsp;<Button className='Button-stop' onClick={handleStop}>RESET</Button>
+    </div>
+    <div className="clock-container">
+      <div className="clock-balls">
+        <div className="clock-timer">
+          <span className="clock-counter"> {seconds} </span>
+          <span className="clock-dayer"> DAYS </span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default (Counter);
