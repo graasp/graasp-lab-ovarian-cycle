ee97e2de51d7754aca3bc999cc3639512b4252ae
Mashkour <azizmashkour@gmail.com>
2018-11-20 17:30:51 +0100

    feat: create ovulation phases component

---
+++ src/components/controls/phases/Phases.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Phases from './Phases';
+
+describe('render phase components tests', () => {
+  it('has one instance of phase', () => {
+    const props = {};
+  });
+});
