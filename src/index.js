d2ba5496a39bf32da3269f4372f4c085968b3f8c
Mashkour <azizmashkour@gmail.com>
2018-11-20 15:56:05 +0100

    fix: import bootstrap library

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
