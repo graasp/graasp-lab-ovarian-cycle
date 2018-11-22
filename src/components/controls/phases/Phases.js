9c1a34ca4cb165f2436cf62d66ec099cb33460db
Mashkour <azizmashkour@gmail.com>
2018-11-22 11:05:41 +0100

    fix: make sure ovulation button is active when clicked

--- src/components/controls/phases/Phases.js
+++ src/components/controls/phases/Phases.js
@@ -2,15 +2,38 @@ import React from 'react';
 import './Phases.css';
 import { Button } from 'reactstrap';
 
-export const Phases = ({ handleOvulation }) => (
+export const Phases = ({
+  handleOvulation,
+  ovulationActive,
+  preOvulation,
+}) => (
   <div className="Phases-Container">
     <div className="phases-title">
       <span className="select-step"> Select step... </span>
     </div>
     <div className="phases">
-      <Button outline color="secondary" className='pre-ovulation' >Pre-Ovulation</Button>
-      <Button outline color="secondary" className='mx-2 ovulation' onClick={handleOvulation} >Ovulation</Button>
-      <Button outline color="secondary" className='post-ovulation' >Post-Ovulation</Button>
+      <Button
+        outline
+        color="secondary"
+        className='pre-ovulation'
+      >
+        Pre-Ovulation
+      </Button>
+      <Button
+        outline
+        color="secondary"
+        className={`${ovulationActive ? 'active-ovulation' : ''} mx-2 ovulation`}
+        onClick={handleOvulation}
+      >
+        Ovulation
+      </Button>
+      <Button
+        outline
+        color="secondary"
+        className='post-ovulation'
+      >
+        Post-Ovulation
+      </Button>
     </div>
   </div>
 );
