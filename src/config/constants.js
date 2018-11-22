e822678a69dcf09eef328dc360ba0d05cd4bb9c9
Mashkour <azizmashkour@gmail.com>
2018-11-22 14:49:49 +0100

    fix: create default consts

---
+++ src/config/constants.js
@@ -0,0 +1,10 @@
+export const MAX_LH = 1085;
+export const SKY_BLUE = '#12a3c1';
+export const GREEN = '#3bc71f';
+export const ORANGE = '#ff7a00';
+export const PURPLE_BLUE = '#9C27B0';
+export const COMMON_DOTS = [414, 1020];
+export const COMMON_DOTS_1 = [415, 930];
+export const COMMON_DOTS_2 = [418, 830];
+export const COMMON_DOTS_3 = [417, 700];
+export const COMMON_DOTS_4 = [413, 470];
