7b2bf4bbfbd52c017064580393edbbd518beed58
Mashkour <azizmashkour@gmail.com>
2018-11-28 11:58:28 +0100

    test: implement calendar component test

---
+++ src/components/controls/counter/Calendar.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Calendar from './Calendar';
+
+describe('<Calendar /> component', () => {
+  const props = {
+    dayCount: 0,
+  };
+
+  const component = shallow(<Calendar {...props} />);
+
+  it('renders correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+  it('has one <svg /> with class fa-calendar', () => {
+    expect(component.find('svg.fa-calendar').length).toEqual(1);
+  });
+});
