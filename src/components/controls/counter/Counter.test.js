22e57e993850e1cacc6c10e506ad9f421ab41c99
Mashkour <azizmashkour@gmail.com>
2018-11-28 12:47:00 +0100

    test: implement counter component test and rebuild it related snapshot

---
+++ src/components/controls/counter/Counter.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Button } from 'reactstrap';
+import Counter from './Counter';
+
+describe('<Counter /> component', () => {
+  const props = {
+    t: jest.fn(),
+    handleStart: jest.fn(),
+    handleStop: jest.fn(),
+    isStarted: false,
+    ovulationActive: false,
+    postOvulationActive: false,
+    preOvulationActive: false,
+    isDisabled: false,
+  };
+
+  const component = shallow(<Counter {...props} />);
+
+  it('renders correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+  it('has one <div /> with class counter-container', () => {
+    expect(component.find('div.counter-container').length).toEqual(1);
+  });
+  it('shows one <Button />', () => {
+    expect(component.find(Button).length).toEqual(1);
+  });
+});
