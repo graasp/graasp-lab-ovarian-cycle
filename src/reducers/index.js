d247c149061a4e1cd3afa6cc5ef5422f98afc3fc
Mashkour <azizmashkour@gmail.com>
2019-01-11 17:38:03 +0100

    fix: import all new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import languageReducer from './language';
+import settingReducer from './setting';
 import svgReducer from './svg';
 // importing and passing our created svg
 // from the svg component and pass it to our react reducer
 export default combineReducers({
   svg: svgReducer,
+  setting: settingReducer,
+  laguange: languageReducer,
 });
