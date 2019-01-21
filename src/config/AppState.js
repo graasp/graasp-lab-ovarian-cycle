9aeabec046157bf3fe1d6415d1a8423cf5024901
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2019-01-21 20:20:10 +0100

    fix: add missing state params

--- src/config/AppState.js
+++ src/config/AppState.js
@@ -21,6 +21,7 @@ import {
 // for all our hormones paths
 // there are located into our ./constants.js file
 export const AppState = {
+  open: false,
   openModal: false,
   themeColor: '#2196F5',
   obserViewActive: true,
