e4658188f2924523eda0e1a575bfedc5a7dceaf6
Mashkour <azizmashkour@gmail.com>
2019-01-22 20:23:21 +0100

    feat: title should be configurable and also add fab icon to toggle lab title

--- src/components/controls/common/SideMe.js
+++ src/components/controls/common/SideMe.js
@@ -11,6 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Fab from '@material-ui/core/Fab';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Visualizer from '../visualizer/Visualizer';
@@ -57,43 +58,60 @@ class PersistentDrawerRight extends React.Component {
       secreteProgest,
       obserViewActive,
       themeColor,
+      showTitle,
       t,
     } = this.props;
+    console.log('this.props;this.props;', this.props);
     const { open } = this.state;
 
     return (
       <div className={classes.root}>
         <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar disableGutters={!open} style={{ backgroundColor: themeColor }}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h4" color="inherit" noWrap>
-              {t('Synchronization of the ovarian cycle')}
-            </Typography>
-          </Toolbar>
-        </AppBar>
+        { showTitle ? (
+          <AppBar
+            position="fixed"
+            className={classNames(classes.appBar, {
+              [classes.appBarShift]: open,
+            })}
+          >
+            <Toolbar disableGutters={!open} style={{ backgroundColor: themeColor }}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h4" color="inherit" noWrap className={classes.title}>
+                {t('Synchronization of the ovarian cycle')}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        ) : ''
+        }
         <main
           className={classNames(classes.content, {
             [classes.contentShift]: open,
           })}
         >
-          <div className={classes.drawerHeader} />
+          { showTitle ? (
+            <div className={classes.drawerHeader} />
+          ) : ''
+          }
           <Calendar
             dayCount={dayCount}
             themeColor={themeColor}
           />
+          <Fab
+            color="primary"
+            aria-label="Add"
+            onClick={this.handleDrawerOpen}
+            className={classes.fab}
+            style={{ backgroundColor: themeColor }}
+          >
+            <MenuIcon style={{ color: 'white' }} />
+          </Fab>
           <Hormones
             ovulation={ovulation}
             postOvulation={postOvulation}
@@ -172,10 +190,12 @@ PersistentDrawerRight.propTypes = {
   secreteProgest: PropTypes.bool.isRequired,
   obserViewActive: PropTypes.bool.isRequired,
   themeColor: PropTypes.string.isRequired,
+  showTitle: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   themeColor: state.setting.themeColor,
+  showTitle: state.setting.showTitle,
 });
 
 const connectedComponent = connect(mapStateToProps)(PersistentDrawerRight);
