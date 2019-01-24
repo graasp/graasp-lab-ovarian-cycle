f396533f23787b0306c2854718d5748d1b6d09d4
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2019-01-24 15:02:16 +0100

    fix: buttons should be disabled when a cycle is playing

--- src/components/controls/phases/Phases.js
+++ src/components/controls/phases/Phases.js
@@ -11,35 +11,47 @@ export const Phases = ({
   ovulationActive,
   preOvulationActive,
   postOvulationActive,
+  preOvulationStep,
+  ovulationStep,
+  postOvulationStep,
   t,
 }) => (
   <div className="phases-container">
     <div className="phases">
       <Button
-        disabled={ovulationActive || postOvulationActive}
+        disabled={
+          ovulationActive || postOvulationActive || preOvulationStep
+          || ovulationStep || postOvulationStep
+        }
         outline
         color="secondary"
-        className={`${preOvulationActive ? 'active-preovulation' : ''} pre-ovulation`}
+        className={`${preOvulationActive ? 'active-preovulation' : ''} ${preOvulationStep ? 'pre-ovulation' : ''} cycle-phase`}
         onClick={handlePreOvulation}
       >
         {t('Pre-Ovulation')}
       </Button>
       <br />
       <Button
-        disabled={preOvulationActive || postOvulationActive}
+        disabled={
+          preOvulationActive || postOvulationActive || preOvulationStep
+          || ovulationStep || postOvulationStep
+        }
         outline
         color="secondary"
-        className={`${ovulationActive ? 'active-ovulation' : ''} mx-2 ovulation`}
+        className={`${ovulationActive ? 'active-ovulation' : ''} ${ovulationStep ? 'ovulation' : ''} m-2 cycle-phase`}
         onClick={handleOvulation}
       >
         {t('Ovulation')}
       </Button>
       <br />
       <Button
-        disabled={ovulationActive || preOvulationActive}
+        disabled={
+          ovulationActive || preOvulationActive || preOvulationStep
+          || ovulationStep || postOvulationStep
+        }
         outline
         color="secondary"
-        className={`${postOvulationActive ? 'active-postovulation' : ''} post-ovulation`}
+        className={`${postOvulationActive ? 'active-postovulation' : ''} ${postOvulationStep ? 'post-ovulation' : ''} cycle-phase`}
         onClick={handlePostOvulation}
       >
         {t('Post-Ovulation')}
@@ -62,6 +74,9 @@ const mapStateToProps = state => ({
   preOvulationActive: state.setting.preOvulationActive,
   postOvulationActive: state.setting.postOvulationActive,
   ovulationActive: state.setting.ovulationActive,
+  preOvulationStep: state.setting.preOvulationStep,
+  ovulationStep: state.setting.ovulationStep,
+  postOvulationStep: state.setting.postOvulationStep,
 });
 
 
